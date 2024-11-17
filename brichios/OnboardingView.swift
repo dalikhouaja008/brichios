70e512b8507c7a73cfb6e17ad81c3f082112b3ad
dalikhouaja008 <mohamedali.khouaja@esprit.tn>
2024-11-17 13:15:12 +0100

    controlle de saisie version 1

---
+++ brichios/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  brichios
+//
+//  Created by Mac Mini 2 on 16/11/2024.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
---
+++ brichios/ui/onBoarding/pageView.swift
@@ -0,0 +1,8 @@
+//
+//  pageView.swift
+//  brichios
+//
+//  Created by Mac Mini 2 on 17/11/2024.
+//
+
+import Foundation
