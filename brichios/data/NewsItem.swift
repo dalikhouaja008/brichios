211c41c6434bbda797a11daec86e6abe00e238cd
raednasri <nasriraed.contact@gmail.com>
2024-12-05 15:54:53 +0100

    validation 05/12

---
+++ brichios/data/NewsItem.swift
@@ -0,0 +1,8 @@
+//
+//  NewsItem.swift
+//  brichios
+//
+//  Created by Mac Mini 2 on 5/12/2024.
+//
+
+import Foundation
---
+++ brichios/ui/ExchangeRate/ExchangeRate_components/newsItemsView.swift
@@ -0,0 +1,8 @@
+//
+//  newsItemsView.swift
+//  brichios
+//
+//  Created by Mac Mini 2 on 5/12/2024.
+//
+
+import Foundation
