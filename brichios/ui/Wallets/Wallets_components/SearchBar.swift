f66406fefae74ed245b691e92bfacdd170bcc05d
dalikhouaja008 <mohamedali.khouaja@esprit.tn>
2024-12-18 22:20:32 +0100

    intégrer partie transaction +avec stats et tnd wallet

---
+++ brichios/ui/Wallets/Wallets_components/SearchBar.swift
@@ -0,0 +1,8 @@
+//
+//  SearchBar.swift
+//  brichios
+//
+//  Created by Mac Mini 2 on 18/12/2024.
+//
+
+import Foundation
---
+++ brichios/ui/Wallets/Wallets_components/Transaction/TransactionStatsHeader.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionStatsHeader.swift
+//  brichios
+//
+//  Created by Mac Mini 2 on 18/12/2024.
+//
+
+import Foundation
